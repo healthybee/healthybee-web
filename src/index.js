7eb7174a619dc7277be4300c126f9b71cdb15f82
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-07-20 10:51:12 +0530

    new dev file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router";
 import registerServiceWorker from './registerServiceWorker';
+import indexRoutes from "./routes";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./assets/scss/material-kit-react.css"
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
