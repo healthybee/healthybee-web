2f8e922c677b8f5eb82d65b68ab06d0eef43b8c0
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-09-10 05:19:04 +0530

    textual fixes

--- src/views/Components/Sections/SectionMainOffer.js
+++ src/views/Components/Sections/SectionMainOffer.js
@@ -21,7 +21,7 @@ class SectionMainOffer extends React.Component {
           <GridContainer className={classes.textCenter} justify="center">
             <GridItem xs={12} sm={12} md={8}>
               <h2>
-                Get Discount <b> 10% </b> on <i className="fas fa-utensils" />{' '}
+                Get <b> 10% </b> Discount on <i className="fas fa-utensils" />{' '}
                 Dine In.
               </h2>
               <h4>
