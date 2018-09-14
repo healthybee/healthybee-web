7708a02c8cc51ef16e0de000691ec0ad2689766c
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-09-14 23:50:28 +0530

    react snap added for performance

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router";
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
-import indexRoutes from "./routes";
+import indexRoutes from './routes';
 
-import "./assets/scss/material-kit-react.css?v=1.1.0";
+import './assets/scss/material-kit-react.css?v=1.1.0';
 
 var hist = createBrowserHistory();
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} key={key} component={prop.component} />
+          );
+        })}
+      </Switch>
+    </Router>,
+    rootElement
+  );
+} else {
+  render(
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} key={key} component={prop.component} />
+          );
+        })}
+      </Switch>
+    </Router>,
+    rootElement
+  );
+}
+
 registerServiceWorker();
