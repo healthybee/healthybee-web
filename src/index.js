645ebb57f3996cbeae93b6d44925898a8d42eb99
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-09-16 01:32:25 +0530

    react snapshot instead of react snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-snapshot';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,34 +8,15 @@ import indexRoutes from './routes';
 import './assets/scss/material-kit-react.css?v=1.1.0';
 
 var hist = createBrowserHistory();
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <Router history={hist}>
-      <Switch>
-        {indexRoutes.map((prop, key) => {
-          return (
-            <Route path={prop.path} key={key} component={prop.component} />
-          );
-        })}
-      </Switch>
-    </Router>,
-    rootElement
-  );
-} else {
-  render(
-    <Router history={hist}>
-      <Switch>
-        {indexRoutes.map((prop, key) => {
-          return (
-            <Route path={prop.path} key={key} component={prop.component} />
-          );
-        })}
-      </Switch>
-    </Router>,
-    rootElement
-  );
-}
+render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
