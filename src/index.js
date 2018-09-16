42077abcc55fdb0e12aa4814fb565372f6a0d47b
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-09-16 12:28:52 +0530

    react snap updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import { hydrate, render } from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,15 +8,34 @@ import indexRoutes from './routes';
 import './assets/scss/material-kit-react.css?v=1.1.0';
 
 var hist = createBrowserHistory();
-render(
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} key={key} component={prop.component} />
+          );
+        })}
+      </Switch>
+    </Router>,
+    rootElement
+  );
+} else {
+  render(
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} key={key} component={prop.component} />
+          );
+        })}
+      </Switch>
+    </Router>,
+    rootElement
+  );
+}
 
 registerServiceWorker();
