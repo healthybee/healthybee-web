c5c0f7dfa3c8029472a8f0986fb01dabbedf968f
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-10-03 03:18:44 +0530

    latest images updates

--- src/views/Components/Sections/SectionCarousel.js
+++ src/views/Components/Sections/SectionCarousel.js
@@ -10,11 +10,16 @@ import GridContainer from '../../../components/Grid/GridContainer.js';
 import GridItem from '../../../components/Grid/GridItem.js';
 import Card from '../../../components/Card/Card.js';
 import carouselStyle from '../../../assets/jss/material-kit-react/views/componentsSections/carouselStyle.js';
-import image4 from '../../../assets/img/food/avocado-cooked.jpeg';
-import image5 from '../../../assets/img/food/buffet-chicken-cooking.jpg';
-import image6 from '../../../assets/img/food/bowl-delicious-diet.jpeg';
-import image7 from '../../../assets/img/food/banana-bread-bread-breakfast.jpeg';
-import image8 from '../../../assets/img/food/bread-breakfast-close-up.jpeg';
+import image4 from '../../../assets/img/food/Green_Tea.jpg';
+import image5 from '../../../assets/img/food/Hot_Coffee.jpg';
+import image6 from '../../../assets/img/food/Masala_Tea.jpg';
+import image7 from '../../../assets/img/food/Ginger_Tea.jpg';
+import image8 from '../../../assets/img/food/Fruit_Museli.jpg';
+import image9 from '../../../assets/img/food/Chicken_SW.jpg';
+import image10 from '../../../assets/img/food/Veg_mayo_SW.jpg';
+import image11 from '../../../assets/img/food/Jerk_Chicken_Salad.jpg';
+import image12 from '../../../assets/img/food/Egg_Brown_Rice.jpg';
+import image13 from '../../../assets/img/food/Fruit_Custard.jpg';
 
 class SectionCarousel extends React.Component {
   render() {
@@ -45,7 +50,7 @@ class SectionCarousel extends React.Component {
                       height="350px"
                     />
                     <div className="slick-caption">
-                      <h3>Peri Peri Grilled Chicken Salad</h3>
+                      <h3>Green Tea</h3>
                     </div>
                   </div>
                   <div>
@@ -56,7 +61,7 @@ class SectionCarousel extends React.Component {
                       height="350px"
                     />
                     <div className="slick-caption">
-                      <h3>Jamacian Grilled Chicken Salad</h3>
+                      <h3>Hot Coffee</h3>
                     </div>
                   </div>
                   <div>
@@ -67,7 +72,7 @@ class SectionCarousel extends React.Component {
                       height="350px"
                     />
                     <div className="slick-caption">
-                      <h3>Fresh Fruit Salad</h3>
+                      <h3>Masala Tea</h3>
                     </div>
                   </div>
                   <div>
@@ -78,7 +83,7 @@ class SectionCarousel extends React.Component {
                       height="350px"
                     />
                     <div className="slick-caption">
-                      <h3>Fruit Museli Breakfast</h3>
+                      <h3>Ginger Tea</h3>
                     </div>
                   </div>
                   <div>
@@ -89,7 +94,62 @@ class SectionCarousel extends React.Component {
                       height="350px"
                     />
                     <div className="slick-caption">
-                      <h3>Boiled Egg Salad</h3>
+                      <h3>Fruit Museli Breakfast</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <img
+                      src={image9}
+                      alt="Third slide"
+                      className="slick-image"
+                      height="350px"
+                    />
+                    <div className="slick-caption">
+                      <h3>Chicken Sandwich</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <img
+                      src={image10}
+                      alt="Third slide"
+                      className="slick-image"
+                      height="350px"
+                    />
+                    <div className="slick-caption">
+                      <h3>Veg Mayo Sandwich</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <img
+                      src={image11}
+                      alt="Third slide"
+                      className="slick-image"
+                      height="350px"
+                    />
+                    <div className="slick-caption">
+                      <h3>Jerk Chicken Salad</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <img
+                      src={image12}
+                      alt="Third slide"
+                      className="slick-image"
+                      height="350px"
+                    />
+                    <div className="slick-caption">
+                      <h3>Egg Brown Rice Salad</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <img
+                      src={image13}
+                      alt="Third slide"
+                      className="slick-image"
+                      height="350px"
+                    />
+                    <div className="slick-caption">
+                      <h3>Fruit Custard</h3>
                     </div>
                   </div>
                 </Carousel>
