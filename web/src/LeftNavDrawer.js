c48287969b7d26c053ef1b5b9ff9c61dd16c851e
Dattatraya Ringe <dattatraya.ringe@cognizant.com>
2020-03-29 19:44:43 +0530

    Deleted unnecessary files

--- web/src/LeftNavDrawer.js
+++ web/src/LeftNavDrawer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
