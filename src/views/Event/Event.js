03e8e97e30309eeea1440c321d0819c199fee1ed
Prasannakumar Kapate <prasanna.1616@gmail.com>
2020-10-28 11:19:35 +0530

    mobile number updated

--- src/views/Event/Event.js
+++ src/views/Event/Event.js
@@ -34,7 +34,7 @@ class Event extends React.Component {
                 <h3> We consume nothing but fresh juices for 2 days.</h3>
                 <h3> </h3>
                 <h4>Coming Soon ..., please call us for more information .</h4>
-                <h4>Mobile No: +91-99756 90709.</h4>
+                <h4>Mobile No: +91-8793580502.</h4>
 
                 <br />
               </GridItem>
