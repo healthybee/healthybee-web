c9abcbf51d7e68eeb5dbb4c5acea926b1512d384
Prasannakumar Kapate <prasanna.1616@gmail.com>
2020-11-07 15:00:42 +0530

    register now added as external link

--- src/views/Event/Event.js
+++ src/views/Event/Event.js
@@ -11,10 +11,9 @@ import Footer from '../../components/Footer/Footer.js';
 import Parallax from '../../components/Parallax/Parallax.js';
 import GridContainer from '../../components/Grid/GridContainer.js';
 import GridItem from '../../components/Grid/GridItem.js';
-// import Button from "../../components/CustomButtons/Button.js";
+import Button from '../../components/CustomButtons/Button.js';
 import landingPageStyle from '../../assets/jss/material-kit-react/views/landingPage.js';
 import EventSection from './EventSection.js';
-import { Button } from '@material-ui/core';
 
 class Event extends React.Component {
   render() {
@@ -23,16 +22,24 @@ class Event extends React.Component {
       <div>
         <Header color="primary" brand="HealthyBee" fixed {...rest} />
 
-        <Parallax
-          small
-          image={require('../../assets/img/food/pexels-event.jpg')}
-        >
+        <Parallax image={require('../../assets/img/food/pexels-event.jpg')}>
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
                 <h1 className={classes.title}>2 Days Detox Program</h1>
-                <br />
-                <Button>Register Now</Button>
+                <h3> We consume nothing but fresh juices for 2 days.</h3>
+                <h4>Coming Soon ..., please call us for more information .</h4>
+                <h4>Mobile No: +91-8793580502.</h4>
+                <Button
+                  color="warning"
+                  size="md"
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSdGs7k79V0rvpeiNfNIm6P6x9hdSXKYn6nhC0heQ_xkNW_CYw/viewform?embedded=true"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  round
+                >
+                  Register Now
+                </Button>
               </GridItem>
             </GridContainer>
           </div>
